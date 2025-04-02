982206127fd04847544a6c8aa148bb7711d20089
IñigoMartin <107561907+delberRepository@users.noreply.github.com>
2025-04-02 20:20:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  base: "/https://delberrepository.github.io/portfolio/",
-};
+export default defineConfig({
+  plugins: [react()],
+  base: "https://delberrepository.github.io/portfolio/",
+})
+
